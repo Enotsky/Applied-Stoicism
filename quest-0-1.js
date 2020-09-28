6b296e09307f94b490066db433661f9e1b4098e8
Elizaveta Oginsky <elizaveta.oginsky@gmail.com>
2020-09-28 21:31:17 +0300

    New font options and further deved script

--- quest-0-1.js
+++ quest-0-1.js
@@ -1,35 +1,92 @@
+// FUNCTIONS
+
+/// Removing the quest block
 function kill(parent, child) {
   parent.removeChild(child);
+}
+
+/// Delaying removal
+function bye(parent, child, text){
+  text.innerHTML = "Ну оооок :(";
+  setTimeout(kill, 2000, parent, child);
   console.log("clicked");
 }
 
-function bye(parent, child, text){
-  text.innerHTML = "Ну оооок :("
-  setTimeout(kill, 2000, parent, child)
+function update(i){
+  text.innerHTML = library[i].question;
+  option1.innerHTML = library[i].answer1;
+  option2.innerHTML = library[i].answer2;
 }
 
 
-var q = "Хочешь, я посоветую упражнение под твою задачу?";
-var op1 = "Давай"
-var op2 = "Да ну не"
 
-var page = document.getElementById('content')
-var block = document.getElementById("quest-block");
+// VARIABLES
+var i = 0;
+var t = "Квест"
+var q = "Могу посоветовать тебе какое-нибудь упражнение из области прикладного "+
+"стоицизма. Для этого нужно ответить на пару вопросов. Хочешь?";
+var op1 = "Давай";
+var op2 = "Да ну не";
 
+// OBJECTS
 
-var text = document.getElementById("question");
-text.innerHTML = q;
-var ansr = document.getElementById("answer");
+/// Addressing the existing objects
+var page = document.getElementById('content');
+var block = document.getElementById('quest-block');
+
+/// Adding new objects
+var title = document.createElement('h2');
+title.classList.add('unit-title');
+block.appendChild(title);
+
+var text = document.createElement("p");
+block.appendChild(text);
 
 var option1 = document.createElement("button");
 option1.classList.add("option-btn");
-option1.innerHTML = op1;
+block.appendChild(option1);
 
 var option2 = document.createElement("button");
 option2.classList.add("option-btn");
-option2.innerHTML = op2;
+block.appendChild(option2);
 
-option2.addEventListener("click", function(){bye(page, block, text)}, false);
+// LIBRARY
+library = [
+  {
+    n: 0,
+    question: "Могу посоветовать тебе какое-нибудь упражнение из области "+
+    "прикладного стоицизма. Для этого нужно ответить на пару вопросов. Хочешь?",
+    answer1: "Давай",
+    answer2: "Да ну не",
+    delta1: 1,
+    delta2: 2
+  },
+  {
+    n: 1,
+    question: "Окей, начнем. Как дела вообще?",
+    answer1: "Всё ровно",
+    answer2: "Ну есть сложности",
+    delta1: 3
+  },
+  {
+    n: 2,
+    question: "Ну ооооок :(",
+    answer1: "Пока",
+    answer2: "Auf Wiedersehen",
+  },
+  {
+    n: 3,
+    question: "Хм, а в чем заключается сложность",
+    answer1: "Не понятно, что делать",
+    answer2: "Надо делать, а я не хочу :("
+  }
 
-ansr.appendChild(option1);
-ansr.appendChild(option2);
+];
+
+// Assining texts
+title.innerHTML = t;
+update(i);
+
+
+option1.addEventListener("click", function (){i = library[i].delta1; update(i)}, false);
+option2.addEventListener("click", function (){bye(page, block, text)}, false);
